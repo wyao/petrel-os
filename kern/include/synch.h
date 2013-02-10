0d2b9acf7743e21cc05dfafe747cd1c8dd9c3372
Willie Yao <wyao13@college.harvard.edu>
2013-02-10 03:29:51 -0500

    Implements CV.

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -113,8 +113,7 @@ void lock_destroy(struct lock *);
 
 struct cv {
 	char *cv_name;
-	// add what you need here
-	// (don't forget to mark things volatile as needed)
+	struct wchan *cv_wchan;
 };
 
 struct cv *cv_create(const char *name);
--- kern/thread/synch.c
+++ kern/thread/synch.c
@@ -257,8 +257,12 @@ cv_create(const char *name)
 		return NULL;
 	}
 
-	// add stuff here as needed
-
+	cv->cv_wchan = wchan_create(cv->cv_name);
+	if (cv->cv_wchan == NULL) {
+		kfree(cv->cv_name);
+		kfree(cv);
+		return NULL;
+	}
 	return cv;
 }
 
@@ -267,8 +271,7 @@ cv_destroy(struct cv *cv)
 {
 	KASSERT(cv != NULL);
 
-	// add stuff here as needed
-
+	wchan_destroy(cv->cv_wchan);
 	kfree(cv->cv_name);
 	kfree(cv);
 }
@@ -276,23 +279,22 @@ cv_destroy(struct cv *cv)
 void
 cv_wait(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	lock_release(lock);
+	wchan_lock(cv->cv_wchan);
+	wchan_sleep(cv->cv_wchan);
+	lock_acquire(lock);
 }
 
 void
 cv_signal(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	KASSERT(lock_do_i_hold(lock));
+	wchan_wakeone(cv->cv_wchan);
 }
 
 void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
-	// Write this
-	(void)cv;    // suppress warning until code gets written
-	(void)lock;  // suppress warning until code gets written
+	KASSERT(lock_do_i_hold(lock));
+	wchan_wakeall(cv->cv_wchan);
 }
