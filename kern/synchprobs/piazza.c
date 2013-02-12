47b6755b3d36581d19aeae219d4cd0e1175f0584
Willie Yao <wyao13@college.harvard.edu>
2013-02-11 19:59:40 -0500

    Implements piazza without memory cleanup.

--- kern/synchprobs/piazza.c
+++ kern/synchprobs/piazza.c
@@ -45,10 +45,15 @@
  */
 struct piazza_question {
   char *pq_answer;
-  struct lock *lk;
+  struct lock *mutex;
+  struct cv *readerQ;
+  struct cv *writerQ;
+  int readers;
+  int writers;
 };
 
 struct piazza_question *questions[NANSWERS] = { 0 };
+struct lock *creation_lock[NANSWERS];
 
 static void
 piazza_print(int id)
@@ -82,12 +87,21 @@ student(void *p, unsigned long which)
     n = random() % NANSWERS;
 
     // If the instructors haven't seen the question yet, try again.
-    if (questions[n] == NULL || questions[n]->lk == NULL) { //TODO is 2nd check best way to do this?
+    lock_acquire(creation_lock[n]);
+    if (questions[n] == NULL) {
+      lock_release(creation_lock[n]);
       --i;
       continue;
     }
+    lock_release(creation_lock[n]);
 
-    lock_acquire(questions[n]->lk);
+    lock_acquire(questions[n]->mutex);
+    while(!(questions[n]->writers==0))
+      cv_wait(questions[n]->readerQ, questions[n]->mutex);
+    questions[n]->readers++;
+    lock_release(questions[n]->mutex);
+
+    /* Start read */
     pos = questions[n]->pq_answer;
     letter = *pos;
 
@@ -101,7 +115,13 @@ student(void *p, unsigned long which)
     if (*pos != '\0') {
       panic("[%d:%d] Inconsistent answer!\n", (int)which, n); //TODO figure out why
     }
-    lock_release(questions[n]->lk);
+
+    /* End read */
+
+    lock_acquire(questions[n]->mutex);
+    if(--(questions[n]->readers) == 0)
+      cv_signal(questions[n]->writerQ, questions[n]->mutex);
+    lock_release(questions[n]->mutex);
   }
 }
 
@@ -136,21 +156,35 @@ instructor(void *p, unsigned long which)
     // Choose a random Piazza question.
     n = random() % NANSWERS;
 
-    // If first instructor to see the question, create and initalize answers
+    // If first instructor to see the question, initalize answers
+    lock_acquire(creation_lock[n]);
     if (questions[n] == NULL) {
       questions[n] = kmalloc(sizeof(struct piazza_question));
-      questions[n]->lk = lock_create("lk"); //TODO: include which?
-      lock_acquire(questions[n]->lk);
+      questions[n]->mutex = lock_create("mutex");
+      questions[n]->readerQ = cv_create("readerQ");
+      questions[n]->writerQ = cv_create("writerQ");
 
       const char *answer = "aaaaaaaaaa"; //TODO: have const here?
-      questions[n]->pq_answer = kstrdup(answer);
+      questions[n]->pq_answer = kstrdup(answer);      
+
+      lock_release(creation_lock[n]);
     }
-    // Update
+
+    // Not the first instructor
     else{
-      lock_acquire(questions[n]->lk);
+      lock_release(creation_lock[n]);
+
+      lock_acquire(questions[n]->mutex);
+      while(!(questions[n]->readers == 0) && (questions[n]->writers ==0))
+        cv_wait(questions[n]->writerQ, questions[n]->mutex);
+      questions[n]->writers++;
+      lock_release(questions[n]->mutex);
+
+      /* Start write */
       pos = questions[n]->pq_answer;
       letter = *pos;
 
+      // Update answer
       if(letter != 'z'){
         while (*(pos) == letter) {
           (*pos)++;
@@ -158,7 +192,7 @@ instructor(void *p, unsigned long which)
         }        
       }
 
-      // Loop back to A's
+      // Loop answer back to A's
       else{
         while (*(pos) == letter) {
           *pos = 'a';
@@ -166,8 +200,16 @@ instructor(void *p, unsigned long which)
         }
       }
     }
+    // Print submitted answer
     piazza_print(n);
-    lock_release(questions[n]->lk);
+
+    /* End write */
+
+    lock_acquire(questions[n]->mutex);
+    questions[n]->writers--;
+    cv_signal(questions[n]->writerQ, questions[n]->mutex);
+    cv_signal(questions[n]->readerQ, questions[n]->mutex);
+    lock_release(questions[n]->mutex);
   }
 }
 
@@ -189,6 +231,11 @@ piazza(int nargs, char **args)
 
   (void)nargs;
   (void)args;
+
+  // Initalize creation locks
+  for(i=0; i<NANSWERS; i++){
+    creation_lock[i] = lock_create("creation lock");
+  }
 
   for (i = 0; i < NSTUDENTS; ++i) {
     thread_fork_or_panic("student", student, NULL, i, NULL);
