e4537597709c23f262b3412cafb07f0bbaaada7c
Willie Yao <wyao13@college.harvard.edu>
2013-03-03 22:42:07 -0500

    exec test

---
+++ user/testbin/usrexec/usrexec.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> /* for fork */
+#include <sys/types.h> /* for pid_t */
+#include <sys/wait.h> /* for wait */
+
+int main()
+{
+    /*Spawn a child to run the program.*/
+    pid_t pid=fork();
+    if (pid==0) { /* child process */
+        static char *argv[2]={ (char *)"echo",NULL};
+        //static char *argv[3]={"echo","Foo is my name.",NULL};
+        execv("/bin/pwd",argv);
+        exit(127); /* only if execv fails */
+    }
+    else { /* pid!=0; parent process */
+        waitpid(pid,0,0); /* wait for child to exit */
+    }
+    return 0;
+}
